925e5c1c858ad395dde24e5f498caaf211674e71
Mariano Ramonda <mariano.ramonda@altoros.com>
2018-07-27 11:40:57 -0300

    add styled-components and basic layout container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  min-height: 100vh;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Layout>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Layout>
     );
   }
 }
