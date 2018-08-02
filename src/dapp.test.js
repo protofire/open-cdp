6d20a1b3306a8b1feb62efeee67dfb219467d5b1
Mariano Ramonda <mariano.ramonda@altoros.com>
2018-08-02 00:56:14 -0300

    add a lot of things...

--- src/dapp.test.js
+++ src/dapp.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Dapp from './dapp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Dapp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
